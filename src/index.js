2c39c3528ec9913cd677443f31acb086d2293c6f
Sourav Banerjee <rjsourav88@gmail.com>
2022-08-19 23:30:11 +0530

    fix1

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-function generateCodeFromObject(obj){
-    //return a code generated string
-   }
-   
-module.exports=generateCodeFromObject;
+// function generateCodeFromObject(obj){
+//     //return a code generated string
+//    }
+
+// module.exports=generateCodeFromObject;
 
 
 // ReactDOM.render(<App />, document.getElementById("root"));
+
+ReactDOM.render(
+    <React.Fragment>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </React.Fragment>,
+    document.getElementById("root")
+);
